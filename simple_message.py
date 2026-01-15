7192d74803ef67d7b1b950767245964b7f031486
Micklevar <alejandrosantosgg1930@gmail.com>
2026-01-14 23:23:21 -0500

    Add solutions for Chapter 2: Variables and Simple Data Types

---
+++ simple_message.py
@@ -0,0 +1,9 @@
+#2-1. Simple Message:
+message = "I love Neon Genesis Evangelion"
+print(message)
+#2-2. Simple messages:
+message_1 = "I love Berserk"
+print(message_1)
+
+message_1 = "I love Oyasumi Punpun"
+print(message_1)
